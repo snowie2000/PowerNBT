9c8abe71953af3c3c0877b18ce903fb015bc3324
snowie2000 <mactype.feedback@gmail.com>
2026-03-07 15:00:45 +0800

    update image and item ids

--- src/components/editor/InventoryInspector.tsx
+++ src/components/editor/InventoryInspector.tsx
@@ -157,6 +157,10 @@ export function parseItem(node: NbtNode): McItem {
     const dmgComp = components && child(components, 'minecraft:damage')
     if (dmgComp && typeof dmgComp.value === 'number') damage = dmgComp.value
   }
+  // Bedrock stores color/data value as Damage directly on the item compound
+  if (damage == null) {
+    damage = numVal(node, 'Damage')
+  }
 
   const isAir = !id || id === 'minecraft:air' || id === 'air'
   return { id, count, slot, customName, lore, enchants, damage, isAir }
@@ -202,22 +206,256 @@ export function isInventoryNode(node: NbtNode): boolean {
 
 // ── ItemIcon ──────────────────────────────────────────────────────────────────
 
-/** Canonical alias → preferred image ID mapping */
+/** Alias (optionally with Bedrock color index `:N`) → canonical image ID */
 const ITEM_ALIASES: Record<string, string> = {
-  'minecraft:bed':                   'minecraft:white_bed',
-  'minecraft:undyed_shulker_box':    'minecraft:shulker_box',
-  'minecraft:dye':                   'minecraft:white_dye',
-  'minecraft:banner':                'minecraft:white_banner',
-  'minecraft:wool':                  'minecraft:white_wool',
+  "minecraft:oak_fence_gate": "minecraft:fence_gate",
+  "minecraft:oak_door": "minecraft:wooden_door",
+  "minecraft:oak_trapdoor": "minecraft:trapdoor",
+  "minecraft:oak_pressure_plate": "minecraft:wooden_pressure_plate",
+  "minecraft:oak_button": "minecraft:wooden_button",
+  "minecraft:block_of_bamboo": "minecraft:bamboo_block",
+  "minecraft:block_of_stripped_bamboo": "minecraft:stripped_bamboo_block",
+  "minecraft:stone_stairs": "minecraft:normal_stone_stairs",
+  "minecraft:stone_slab": "minecraft:stone_block_slab4:2",
+  "minecraft:cobblestone_stairs": "minecraft:stone_stairs",
+  "minecraft:mossy_cobblestone_slab": "minecraft:stone_block_slab2:5",
+  "minecraft:mossy_cobblestone_wall": "minecraft:cobblestone_wall:1",
+  "minecraft:stone_bricks": "minecraft:stonebrick",
+  "minecraft:cracked_stone_bricks": "minecraft:stonebrick:2",
+  "minecraft:stone_brick_wall": "minecraft:cobblestone_wall:7",
+  "minecraft:chiseled_stone_bricks": "minecraft:stonebrick:3",
+  "minecraft:mossy_stone_bricks": "minecraft:stonebrick:1",
+  "minecraft:mossy_stone_brick_slab": "minecraft:stone_block_slab4",
+  "minecraft:mossy_stone_brick_wall": "minecraft:cobblestone_wall:8",
+  "minecraft:granite_slab": "minecraft:stone_block_slab3:6",
+  "minecraft:granite_wall": "minecraft:cobblestone_wall:2",
+  "minecraft:polished_granite_slab": "minecraft:stone_block_slab3:7",
+  "minecraft:diorite_slab": "minecraft:stone_block_slab3:4",
+  "minecraft:diorite_wall": "minecraft:cobblestone_wall:3",
+  "minecraft:polished_diorite_slab": "minecraft:stone_block_slab3:5",
+  "minecraft:andesite_slab": "minecraft:stone_block_slab3:3",
+  "minecraft:andesite_wall": "minecraft:cobblestone_wall:4",
+  "minecraft:polished_andesite_slab": "minecraft:stone_block_slab3:2",
+  "minecraft:bricks": "minecraft:brick_block",
+  "minecraft:brick_wall": "minecraft:cobblestone_wall:6",
+  "minecraft:sandstone_wall": "minecraft:cobblestone_wall:5",
+  "minecraft:chiseled_sandstone": "minecraft:sandstone:1",
+  "minecraft:smooth_sandstone": "minecraft:sandstone:3",
+  "minecraft:smooth_sandstone_slab": "minecraft:stone_block_slab2:6",
+  "minecraft:cut_sandstone": "minecraft:sandstone:2",
+  "minecraft:cut_sandstone_slab": "minecraft:stone_block_slab4:3",
+  "minecraft:red_sandstone_slab": "minecraft:stone_block_slab2",
+  "minecraft:red_sandstone_wall": "minecraft:cobblestone_wall:12",
+  "minecraft:chiseled_red_sandstone": "minecraft:red_sandstone:1",
+  "minecraft:smooth_red_sandstone": "minecraft:red_sandstone:3",
+  "minecraft:smooth_red_sandstone_slab": "minecraft:stone_block_slab3:1",
+  "minecraft:cut_red_sandstone": "minecraft:red_sandstone:2",
+  "minecraft:cut_red_sandstone_slab": "minecraft:stone_block_slab4:4",
+  "minecraft:prismarine_slab": "minecraft:stone_block_slab2:2",
+  "minecraft:prismarine_wall": "minecraft:cobblestone_wall:11",
+  "minecraft:prismarine_bricks": "minecraft:prismarine:2",
+  "minecraft:prismarine_brick_stairs": "minecraft:prismarine_bricks_stairs",
+  "minecraft:prismarine_brick_slab": "minecraft:stone_block_slab2:4",
+  "minecraft:dark_prismarine": "minecraft:prismarine:1",
+  "minecraft:dark_prismarine_slab": "minecraft:stone_block_slab2:3",
+  "minecraft:nether_bricks": "minecraft:nether_brick",
+  "minecraft:nether_brick_wall": "minecraft:cobblestone_wall:9",
+  "minecraft:red_nether_bricks": "minecraft:red_nether_brick",
+  "minecraft:red_nether_brick_slab": "minecraft:stone_block_slab2:7",
+  "minecraft:red_nether_brick_wall": "minecraft:cobblestone_wall:13",
+  "minecraft:end_stone_bricks": "minecraft:end_bricks",
+  "minecraft:end_stone_brick_stairs": "minecraft:end_brick_stairs",
+  "minecraft:end_stone_brick_slab": "minecraft:stone_block_slab3",
+  "minecraft:end_stone_brick_wall": "minecraft:cobblestone_wall:10",
+  "minecraft:purpur_pillar": "minecraft:purpur_block:2",
+  "minecraft:purpur_slab": "minecraft:stone_block_slab2:1",
+  "minecraft:block_of_coal": "minecraft:coal_block",
+  "minecraft:block_of_iron": "minecraft:iron_block",
+  "minecraft:block_of_gold": "minecraft:gold_block",
+  "minecraft:block_of_redstone": "minecraft:redstone_block",
+  "minecraft:block_of_emerald": "minecraft:emerald_block",
+  "minecraft:block_of_lapis_lazuli": "minecraft:lapis_block",
+  "minecraft:block_of_diamond": "minecraft:diamond_block",
+  "minecraft:block_of_netherite": "minecraft:netherite_block",
+  "minecraft:block_of_quartz": "minecraft:quartz_block",
+  "minecraft:chiseled_quartz_block": "minecraft:quartz_block:1",
+  "minecraft:quartz_pillar": "minecraft:quartz_block:2",
+  "minecraft:smooth_quartz_block": "minecraft:quartz_block:3",
+  "minecraft:smooth_quartz_slab": "minecraft:stone_block_slab4:1",
+  "minecraft:block_of_amethyst": "minecraft:amethyst_block",
+  "minecraft:block_of_copper": "minecraft:copper_block",
+  "minecraft:waxed_block_of_copper": "minecraft:waxed_copper",
+  "minecraft:terracotta": "minecraft:hardened_clay",
+  "minecraft:light_gray_glazed_terracotta": "minecraft:silver_glazed_terracotta",
+  "minecraft:shulker_box": "minecraft:undyed_shulker_box",
+  "minecraft:white_bed": "minecraft:bed",
+  "minecraft:light_gray_bed": "minecraft:bed:8",
+  "minecraft:gray_bed": "minecraft:bed:7",
+  "minecraft:black_bed": "minecraft:bed:15",
+  "minecraft:brown_bed": "minecraft:bed:12",
+  "minecraft:red_bed": "minecraft:bed:14",
+  "minecraft:orange_bed": "minecraft:bed:1",
+  "minecraft:yellow_bed": "minecraft:bed:4",
+  "minecraft:lime_bed": "minecraft:bed:5",
+  "minecraft:green_bed": "minecraft:bed:13",
+  "minecraft:cyan_bed": "minecraft:bed:9",
+  "minecraft:light_blue_bed": "minecraft:bed:3",
+  "minecraft:blue_bed": "minecraft:bed:11",
+  "minecraft:purple_bed": "minecraft:bed:10",
+  "minecraft:magenta_bed": "minecraft:bed:2",
+  "minecraft:pink_bed": "minecraft:bed:6",
+  "minecraft:white_banner": "minecraft:banner:15",
+  "minecraft:light_gray_banner": "minecraft:banner:7",
+  "minecraft:gray_banner": "minecraft:banner:8",
+  "minecraft:black_banner": "minecraft:banner",
+  "minecraft:brown_banner": "minecraft:banner:3",
+  "minecraft:red_banner": "minecraft:banner:1",
+  "minecraft:orange_banner": "minecraft:banner:14",
+  "minecraft:yellow_banner": "minecraft:banner:11",
+  "minecraft:lime_banner": "minecraft:banner:10",
+  "minecraft:green_banner": "minecraft:banner:2",
+  "minecraft:cyan_banner": "minecraft:banner:6",
+  "minecraft:light_blue_banner": "minecraft:banner:12",
+  "minecraft:blue_banner": "minecraft:banner:4",
+  "minecraft:purple_banner": "minecraft:banner:5",
+  "minecraft:magenta_banner": "minecraft:banner:13",
+  "minecraft:pink_banner": "minecraft:banner:9",
+  "minecraft:dirt_path": "minecraft:grass_path",
+  "minecraft:coarse_dirt": "minecraft:dirt:1",
+  "minecraft:rooted_dirt": "minecraft:dirt_with_roots",
+  "minecraft:red_sand": "minecraft:sand:1",
+  "minecraft:snow_block": "minecraft:snow",
+  "minecraft:snow": "minecraft:snow_layer",
+  "minecraft:magma_block": "minecraft:magma",
+  "minecraft:lapis_lazuli_ore": "minecraft:lapis_ore",
+  "minecraft:deepslate_lapis_lazuli_ore": "minecraft:deepslate_lapis_ore",
+  "minecraft:nether_quartz_ore": "minecraft:quartz_ore",
+  "minecraft:block_of_raw_iron": "minecraft:raw_iron_block",
+  "minecraft:block_of_raw_copper": "minecraft:raw_copper_block",
+  "minecraft:block_of_raw_gold": "minecraft:raw_gold_block",
+  "minecraft:mushroom_stem": "minecraft:brown_mushroom_block:15",
+  "minecraft:flowering_azalea_leaves": "minecraft:azalea_leaves_flowered",
+  "minecraft:dead_bush": "minecraft:deadbush",
+  "minecraft:dandelion": "minecraft:yellow_flower",
+  "minecraft:vines": "minecraft:vine",
+  "minecraft:small_dripleaf": "minecraft:small_dripleaf_block",
+  "minecraft:frogspawn": "minecraft:frog_spawn",
+  "minecraft:lily_pad": "minecraft:waterlily",
+  "minecraft:wet_sponge": "minecraft:sponge:1",
+  "minecraft:melon": "minecraft:melon_block",
+  "minecraft:jack_o'lantern": "minecraft:lit_pumpkin",
+  "minecraft:hay_bale": "minecraft:hay_block",
+  "minecraft:slime_block": "minecraft:slime",
+  "minecraft:block_of_resin": "minecraft:resin_block",
+  "minecraft:cobweb": "minecraft:web",
+  "minecraft:stonecutter": "minecraft:stonecutter_block",
+  "minecraft:chipped_anvil": "minecraft:anvil:4",
+  "minecraft:damaged_anvil": "minecraft:anvil:8",
+  "minecraft:note_block": "minecraft:noteblock",
+  "minecraft:item_frame": "minecraft:frame",
+  "minecraft:glow_item_frame": "minecraft:glow_frame",
+  "minecraft:skeleton_skull": "minecraft:skull",
+  "minecraft:wither_skeleton_skull": "minecraft:skull:1",
+  "minecraft:player_head": "minecraft:skull:3",
+  "minecraft:zombie_head": "minecraft:skull:2",
+  "minecraft:creeper_head": "minecraft:skull:4",
+  "minecraft:piglin_head": "minecraft:skull:6",
+  "minecraft:dragon_head": "minecraft:skull:5",
+  "minecraft:eye_of_ender": "minecraft:ender_eye",
+  "minecraft:infested_stone": "minecraft:monster_egg",
+  "minecraft:infested_cobblestone": "minecraft:monster_egg:1",
+  "minecraft:infested_stone_bricks": "minecraft:monster_egg:2",
+  "minecraft:infested_mossy_stone_bricks": "minecraft:monster_egg:3",
+  "minecraft:infested_cracked_stone_bricks": "minecraft:monster_egg:4",
+  "minecraft:infested_chiseled_stone_bricks": "minecraft:monster_egg:5",
+  "minecraft:redstone_dust": "minecraft:redstone",
+  "minecraft:redstone_repeater": "minecraft:repeater",
+  "minecraft:redstone_comparator": "minecraft:comparator",
+  "minecraft:powered_rail": "minecraft:golden_rail",
+  "minecraft:minecart_with_hopper": "minecraft:hopper_minecart",
+  "minecraft:minecart_with_chest": "minecraft:chest_minecart",
+  "minecraft:minecart_with_tnt": "minecraft:tnt_minecart",
+  "minecraft:oak_boat_with_chest": "minecraft:oak_chest_boat",
+  "minecraft:bamboo_raft_with_chest": "minecraft:bamboo_chest_raft",
+  "minecraft:bucket_of_cod": "minecraft:cod_bucket",
+  "minecraft:bucket_of_salmon": "minecraft:salmon_bucket",
+  "minecraft:bucket_of_tropical_fish": "minecraft:tropical_fish_bucket",
+  "minecraft:bucket_of_pufferfish": "minecraft:pufferfish_bucket",
+  "minecraft:bucket_of_axolotl": "minecraft:axolotl_bucket",
+  "minecraft:bucket_of_tadpole": "minecraft:tadpole_bucket",
+  "minecraft:book_and_quill": "minecraft:writable_book",
+  "minecraft:spruce_boat_with_chest": "minecraft:spruce_chest_boat",
+  "minecraft:birch_boat_with_chest": "minecraft:birch_chest_boat",
+  "minecraft:jungle_boat_with_chest": "minecraft:jungle_chest_boat",
+  "minecraft:acacia_boat_with_chest": "minecraft:acacia_chest_boat",
+  "minecraft:dark_oak_boat_with_chest": "minecraft:dark_oak_chest_boat",
+  "minecraft:mangrove_boat_with_chest": "minecraft:mangrove_chest_boat",
+  "minecraft:cherry_boat_with_chest": "minecraft:cherry_chest_boat",
+  "minecraft:pale_oak_boat_with_chest": "minecraft:pale_oak_chest_boat",
+  "minecraft:13_disc": "minecraft:music_disc_13",
+  "minecraft:cat_disc": "minecraft:music_disc_cat",
+  "minecraft:blocks_disc": "minecraft:music_disc_blocks",
+  "minecraft:chirp_disc": "minecraft:music_disc_chirp",
+  "minecraft:far_disc": "minecraft:music_disc_far",
+  "minecraft:mall_disc": "minecraft:music_disc_mall",
+  "minecraft:mellohi_disc": "minecraft:music_disc_mellohi",
+  "minecraft:stal_disc": "minecraft:music_disc_stal",
+  "minecraft:strad_disc": "minecraft:music_disc_strad",
+  "minecraft:ward_disc": "minecraft:music_disc_ward",
+  "minecraft:11_disc": "minecraft:music_disc_11",
+  "minecraft:music_disc": "minecraft:music_disc_precipice",
+  "minecraft:wait_disc": "minecraft:music_disc_wait",
+  "minecraft:otherside_disc": "minecraft:music_disc_otherside",
+  "minecraft:relic_music_disc": "minecraft:music_disc_relic",
+  "minecraft:5_disc": "minecraft:music_disc_5",
+  "minecraft:pigstep_disc": "minecraft:music_disc_pigstep",
+  "minecraft:tears_disc": "minecraft:music_disc_tears",
+  "minecraft:lava_chicken_disc": "minecraft:music_disc_lava_chicken",
+  "minecraft:leather_cap": "minecraft:leather_helmet",
+  "minecraft:leather_tunic": "minecraft:leather_chestplate",
+  "minecraft:leather_pants": "minecraft:leather_leggings",
+  "minecraft:turtle_shell": "minecraft:turtle_helmet",
+  "minecraft:tipped_arrow": "minecraft:arrow",
+  "minecraft:raw_beef": "minecraft:beef",
+  "minecraft:steak": "minecraft:cooked_beef",
+  "minecraft:raw_porkchop": "minecraft:porkchop",
+  "minecraft:raw_mutton": "minecraft:mutton",
+  "minecraft:raw_chicken": "minecraft:chicken",
+  "minecraft:raw_rabbit": "minecraft:rabbit",
+  "minecraft:raw_cod": "minecraft:cod",
+  "minecraft:raw_salmon": "minecraft:salmon",
+  "minecraft:nether_quartz": "minecraft:quartz",
+  "minecraft:wheat_crops": "minecraft:wheat",
+  "minecraft:slimeball": "minecraft:slime_ball",
+  "minecraft:nether_brick": "minecraft:netherbrick",
+  "minecraft:dragon's_breath": "minecraft:dragon_breath",
+  "minecraft:rabbit's_foot": "minecraft:rabbit_foot",
+  "minecraft:flower_charge_banner_pattern": "minecraft:flower_banner_pattern",
+  "minecraft:creeper_charge_banner_pattern": "minecraft:creeper_banner_pattern",
+  "minecraft:skull_charge_banner_pattern": "minecraft:skull_banner_pattern",
+  "minecraft:thing_banner_pattern": "minecraft:mojang_banner_pattern",
+  "minecraft:snout_banner_pattern": "minecraft:piglin_banner_pattern",
+  "minecraft:smithing_template": "minecraft:bolt_armor_trim_smithing_template",
+  "minecraft:bottle_o'_enchanting": "minecraft:experience_bottle",
+  "minecraft:monster_spawner": "minecraft:mob_spawner",
+  "minecraft:zombified_piglin_spawn_egg": "minecraft:zombie_pigman_spawn_egg",
+  "minecraft:minecart_with_command_block": "minecraft:command_block_minecart",
+  "minecraft:map": "minecraft:filled_map",
+  "minecraft:jigsaw_block": "minecraft:jigsaw",
+  "minecraft:light": "minecraft:light_block"
 }
 
-const ItemIcon = memo(function ItemIcon({ id, size = 32 }: { id: string; size?: number }) {
+const ItemIcon = memo(function ItemIcon({ id, size = 32, damage }: { id: string; size?: number; damage?: number | null }) {
   if (!id || id === 'minecraft:air' || id === 'air') {
     return <div style={{ width: size, height: size, flexShrink: 0, background: '#1c1c1c', border: '1px solid #333', boxSizing: 'border-box' }} />
   }
 
+  // Try damage-keyed color variant first (Bedrock stores color index in Damage)
+  const colorKey = damage != null && damage > 0 ? `${id}:${damage}` : null
+  const resolvedColorId = colorKey ? (ITEM_ALIASES[colorKey] ?? colorKey) : null
   const resolvedId = ITEM_ALIASES[id] ?? id
-  const src = itemImage[resolvedId]
+  const src = (resolvedColorId ? itemImage[resolvedColorId] : null) ?? itemImage[resolvedId]
+
   if (src) {
     return (
       <img
@@ -271,7 +509,7 @@ function TooltipCard({ item }: { item: McItem }) {
     }}>
       {/* Name + icon row */}
       <div style={{ display: 'flex', alignItems: 'center', gap: 8, marginBottom: 2 }}>
-        <ItemIcon id={item.id} size={32} />
+        <ItemIcon id={item.id} size={32} damage={item.damage} />
         <div style={{ minWidth: 0 }}>
           {nameNode}
           {item.customName && (
@@ -339,7 +577,7 @@ const SlotCell = memo(function SlotCell({
     >
       {item && !item.isAir && (
         <>
-          <ItemIcon id={item.id} size={28} />
+          <ItemIcon id={item.id} size={28} damage={item.damage} />
           {item.count > 1 && (
             <span style={{
               position: 'absolute', bottom: 1, right: 2,
@@ -623,7 +861,7 @@ function EnchantmentEditor({ itemNode, fileIndex }: EnchantmentEditorProps) {
           <div key={e.compoundKey} style={{ display: 'flex', alignItems: 'center', gap: 6 }}>
             <span style={{ color: '#9999EE', fontSize: 13, flex: 1 }}>{e.name}</span>
             <InputNumber
-              min={1} max={255} size="small"
+              min={1} max={32767} size="small"
               value={e.lvl}
               onChange={v => v != null && e.lvlKey && onLevelChange(e.lvlKey, v)}
               style={{ width: 58 }}
@@ -653,7 +891,7 @@ function EnchantmentEditor({ itemNode, fileIndex }: EnchantmentEditorProps) {
           popupMatchSelectWidth={false}
         />
         <InputNumber
-          min={1} max={255} value={addLvl}
+          min={1} max={32767} value={addLvl}
           onChange={v => v != null && setAddLvl(v)}
           size="small" style={{ width: 58 }}
         />
